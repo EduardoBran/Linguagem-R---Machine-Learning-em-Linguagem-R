1ccd66f35aeec49ad6633d395925cbbd7ff86fe8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-09-25 18:06:36 -0300

    conceitos sobre Machine Learning - parte 1

---
+++ 01-Conceito.R
@@ -0,0 +1,70 @@
+# Lista de Exercícios
+
+# Configurando o diretório de trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/11.Machine-Learning-em-Linguagem-R")
+getwd()
+
+
+# O que vamos aprender neste capítulo:
+
+# -> Definição de Machine Learning
+# -> Frameworks de Machine Learning
+# -> Processo de Aprendizagem
+# -> Treinamento, Validação e Teste
+# -> Modelos Preditivos
+# -> Algoritmos de Machine Learning
+# -> Regressão e Classificação Através de 2 Projetos Completos
+# -> Lista de Exercícios com a Construção de Modelos
+
+
+# Iremos trabalhar com 2 Projetos Inteiros de Regressão e Classificação ao longo do capítulo:
+
+#  -> Projeto Prevendo Despesas Hospitalares
+#  -> Projeto Prevendo a Ocorrência de Câncer
+
+# Nestes projetos iremos: definir o problema de negócio, realizar a análise exploratória, transformação e limpeza dos dados, treinar o modelo e
+#                         entregar a solução final.
+
+
+
+
+
+#######################      INTRODUÇÃO AO APRENDIZADO DE MÁQUINA (Machine Learning)      #######################
+
+
+# - Objetivo é aprender o que é e o que não é Machine Learning.
+
+
+# O que é Machine Learning ?
+
+#  -> Machine Learning é um método de análise de dados que automatiza a construção de modelos analíticos usando algoritmos que imperativamente
+#     aprendem a partir dos dados, portando Machine Learning permite que computadores encontrem insights ocultos sem que sejam programados para isso.
+
+#  -> Os algoritmos processam os dados de maneira específicas e criam previsões baseados em padrões detectados nos dados.
+
+
+# E como as máquinas aprendem ?
+
+#  -> As máquinas aprendem através de um processo de treinamento supervisionado em que algoritmos de Machine Learning aprendem a partir de dados, 
+#     ajustando seus parâmetros internos para fazer previsões ou tomar decisões com base em padrões detectados nos dados de treinamento. 
+#     Esse processo é iterativo e envolve a coleta de dados, a preparação, o treinamento, a validação e a aplicação do modelo para resolver 
+#     problemas do mundo real.
+
+
+
+## Inteligência Artificial x Machine Learning x Deep Learning
+
+# - Machine Learning é um subcampo da Inteligência Artificial e o Deep Learning é uma categoria de Machine Learning.
+#   Nós temos várias algoritmos de Machine Learning e uma família de algoritmos é o que chamamos de Deep Learning.
+
+# - Portanto Inteligência Artificial inclui Machine Learning, mas Machine Learning por si só não define Inteligência Artificial.
+
+# - Inteligência Aritificial é baseada em Machine Learning e Machine Learning é essencialmente diferente de Estatística.
+
+
+
+## Machine Learning x Estatística
+
+
+
+
